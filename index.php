e5baa08d587206bff82143115429b48d114e19da
Stephanie Rimel <srimel1@student.gsu.edu>
2020-12-02 03:01:13 -0500

    //TODO - win sequence //TODO - cookies for leader board //TODO - get the high scores and users and display leaderboard using cookies and sessions if you want //TODO - calculate the score by number of moves if you want to
    Finished navigation and implemented with the board

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include("test.html");?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Fifteen Puzzle</title>
+<!--    <link rel="stylesheet" href="stylesheet.css"/>-->
+</head>
+
+</html>
